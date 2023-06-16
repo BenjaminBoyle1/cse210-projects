357767c1a3d021c8eb46020dc0a49f1d11d82a1d
BenjaminBoyle1 <boyle153@gmail.com>
2023-06-15 20:46:33 -0600

    Journal App

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,55 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        while (true)
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GeneratePrompt();
+                Console.WriteLine("Prompt: " + prompt);
+                Console.Write("Response: ");
+                string response = Console.ReadLine();
+                string date = DateTime.Now.ToString("MM/dd/yyyy");
+                journal.AddEntry(prompt, response, date);
+                Console.WriteLine("Entry added successfully.");
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayEntries();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Enter the filename to save: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Enter the filename to load: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (choice == 5)
+            {
+                break;
+            }
+
+            Console.WriteLine();
+        }
     }
 }
